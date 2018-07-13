aa32a16e0b5bd90fb2280cabd1de7ca37b304532
Ollie Badger <ollie.badger@aldautomotive.com>
2018-07-13 15:11:57 +0100

    Section 3

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GradeBook.Enums;
 
@@ -10,6 +11,34 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var dropCount = Students.Count / 20 * 100;
+            List<double> averageGradesList = new List<double>();
+            foreach (var student in Students)
+            {
+                averageGradesList.Add(student.AverageGrade);
+            }
+
+            averageGradesList.Sort();
+
+            if (averageGrade <= dropCount)
+                return 'A';
+            else if (averageGrade <= dropCount *2)
+                return 'B';
+            else if (averageGrade <= dropCount *3)
+                return 'C';
+            else if (averageGrade <= dropCount *4)
+                return 'D';
+            else
+                return 'F';
         }
     }
 }
